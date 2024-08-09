4684a09d98d3b26824b610ffbba69d03909b9cf3
dmswl6310 <dmswl6310@gmail.com>
2024-08-09 16:26:51 +0900

    feat: id와 매칭 스트링으로 가져오기

--- apis/todos-no-rls.ts
+++ apis/todos-no-rls.ts
@@ -14,3 +14,26 @@ export const getTodos = async () => {
     });
   return result.data;
 };
+
+// todoList 가져오기 + by Id
+export const getTodosById = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todo_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id);
+  return result.data;
+};
+
+// todoList 가져오기 + search
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todo_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}%`)
+    .order("id", { ascending: false });
+  return result.data;
+};
