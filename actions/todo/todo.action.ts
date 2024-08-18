9f5547efb9a998b55b32448ef2e6c43e65170d42
dmswl6310 <dmswl6310@gmail.com>
2024-08-18 14:38:06 +0900

    feat: server action을 통해 todolist 기능 동작시키기

--- actions/todo/todo.action.ts
+++ actions/todo/todo.action.ts
@@ -1,17 +1,81 @@
 "use server";
-
 import { createServerSideClient } from "@/lib/supabase";
 
-export const getTodoAction=async()=>{
-    const supabase=await createServerSideClient();
-    const result = await supabase
-    .from("todo_no_rls")
+// todoList 가져오기
+export const getTodos = async () => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
     .select("*")
     .is("deleted_at", null)
     .order("id", {
       ascending: false,
     });
+  return result.data;
+};
 
-    console.log("getTodoaction income",result)
-    return result.data;
-}
+// todoList 가져오기 + by Id
+export const getTodosById = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id);
+  return result.data;
+};
+
+// todoList 가져오기 + search
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}%`)
+    .order("id", { ascending: false })
+    .limit(500);
+  return result.data;
+};
+
+// todoList 생성하기
+export const createTodos = async (content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .insert({ content })
+    .select();
+  return result.data;
+};
+
+// todoList 업데이트 하기
+export const updateTodos = async (id: number, content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({ content, updated_at: new Date().toISOString() })
+    .eq("id", id)
+    .select();
+  return result.data;
+};
+
+// todoList softDelete
+export const deleteTodosSoft = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({
+      deleted_at: new Date().toISOString(),
+      // delete시 update시간 기록은 선택사항
+    })
+    .eq("id", id)
+    .select();
+  return result.data;
+};
+
+// todoList hardDelete
+export const deleteTodosHard = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase.from("todos_with_rls").delete().eq("id", id);
+  return result.data;
+};
